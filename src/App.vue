798fb0ceed91db30e7bb03de07fed2e74b0f6ffb
Emma Barash <emma.barash.dev@gmail.com>
2019-06-18 22:47:57 -0600

    movie db project without the api

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app" v-if="status === 'ready'">
+    <!-- the shorthand for "v-bind" is just ":" -->
+    <Header :title="title" v-if="status === 'ready'"/>
+    <MoviesList />
+  </div>
+  <div v-else-if="status === 'not ready'">
+    Loading
+  </div>
+  <div v-else>
+    Error
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
+import MoviesList from './components/MoviesList.vue'
+
+export default {
+  name: 'app',
+  data(){
+    return {
+      hello: "Hello World",
+      title: "Vue Movie DB",
+      isTrue: true,
+      status: 'ready'
+    }
+  },
+  components: {
+    HelloWorld,
+    Header,
+    MoviesList
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+ul{
+  list-style: none;
+}
+</style>
---
+++ src/components/MoviesList.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+    {{ movie.title }}
+    </div>
+</template>
+<script>
+import { async } from 'q';
+export default {
+    name: 'Movies List',
+    // this is the default state
+    data() {
+        return {
+            movie: {
+
+            }
+        }
+    },
+    // this is a lifecycle method!
+    created: function(){
+        this.fetchData();
+    },
+    methods: {
+        // with an async you need both a "try" and a "catch"
+        fetchData: async function() {
+            try{
+                // await the data to return from the API
+                const res = await fetch(
+                    '
+                    );
+                const movie = await res.json();
+                this.movie = movie;
+            } catch (e){
+                console.log(e);
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+    header{
+        background-color: #111;
+        padding: 20px;
+        color: white;
+    }
+    h1{
+        margin: 0;
+    }
+</style>
+
